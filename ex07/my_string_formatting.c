8bdb55237bfbf082020c2f5d0a8a6b7bc2950c2d
bhekumuzi nkompilo <nkompilo_b@qwasar.io>
2022-01-19 17:11:21 +0000

    created string format function in C

---
+++ ex07/my_string_formatting.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+char my_string_formatting(char* fname, char* lname, int age){
+  fname[20];
+  lname[20];
+  age > 0;
+  printf("Hello, my name is %s %s, I'm %d.\n", fname, lname, age);
+  return 0;
+}
+
+// int main(){
+//    my_string_formatting("bhekie", "nkompilo", 18);
+//    return 0;
+// }
