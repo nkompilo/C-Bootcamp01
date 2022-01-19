f306eff136d3fd759a8afb1eb2451438cb150d62
bhekumuzi nkompilo <nkompilo_b@qwasar.io>
2022-01-19 16:02:12 +0000

    created subtraction function in C

---
+++ ex05/my_sub.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int my_sub(int param_1, int param_2){
+    int diff;
+    diff = param_1 - param_2;
+    return diff;
+}
+
+// int main(){
+//     printf("%d", my_sub(5, 3));
+//     return 0;
+// }
