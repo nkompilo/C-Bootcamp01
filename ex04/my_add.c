958adbe8c9ee58a0aa911f8a42547c26f941961e
bhekumuzi nkompilo <nkompilo_b@qwasar.io>
2022-01-19 16:04:30 +0000

    created addition function in C

---
+++ ex04/my_add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int my_add(int p1, int p2){
+   int sum;
+   sum = p1 +p2;
+   return sum;
+}
+
+// int main(){
+//    printf("%d", my_add(3,5)) ;
+//    return 0;
+// }
