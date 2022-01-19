9ec17b1c5e7e889ce936493ae09a64dff3a61d76
bhekumuzi nkompilo <nkompilo_b@qwasar.io>
2022-01-19 16:11:34 +0000

    created multiplication function in C

---
+++ ex06/my_mult.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int my_mult(int p1, int p2){
+int prod;
+   prod = p1 * p2;
+   return prod;
+}
+
+// int main(){
+//   printf("%d", my_mult(0, 5));
+//   return 0;
+// }
