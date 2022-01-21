419c33baff6a5ffaa8f21a9aa7288ac9d182d638
bhekumuzi nkompilo <nkompilo_b@qwasar.io>
2022-01-21 20:03:37 +0000

    created upcase string function

--- ex09/my_upcase.c
+++ ex09/my_upcase.c
@@ -5,16 +5,16 @@
 char* my_upcase(char* ch){
    for(int i = 0; ch[i] != '\0'; i++){
        if(ch[i] >= 'a' && ch[i] <= 'z'){
-       ch[i] -= 32;}   
-   } return ch;
+          ch[i] -= 32;}   
+   }return ch;
 } 
 
 
 
-int main() {
-  char *my_str = strdup("AbcE Fgef1");
+// int main() {
+//   char *my_str = strdup("AbcE Fgef1");
   
-  printf("RANDOM CASE -> %s\n", my_str);
-  printf("UPCASE      -> %s\n", my_upcase(my_str));
-  return 0;
-}
+//   printf("RANDOM CASE -> %s\n", my_str);
+//   printf("UPCASE      -> %s\n", my_upcase(my_str));
+//   return 0;
+// }
